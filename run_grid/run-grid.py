88b327c5091e054d6771da2a259ec57161ce6981
Thomas Capelle <tcapelle@pm.me>
2022-03-04 11:14:25 +0100

    initial refactor

--- run_grid/run-grid.py
+++ run_grid/run-grid.py
@@ -16,8 +16,8 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 #GLOBALS
 TRAIN_CONFIG = './train.yaml'
 SWEEP_CONFIG = './sweep_config.yaml'
-
 WANDB_PROJECT_NAME = "aws_eks_elastic_demo"
+SWEEP_ID = "z"
 
 
 with open(SWEEP_CONFIG) as file:
@@ -27,18 +27,26 @@ with open(TRAIN_CONFIG) as file:
     train_config = ryaml.load(file)
 
 #create sweep controller
-sweep_id = wandb.sweep(sweep_config, project=WANDB_PROJECT_NAME)
+SWEEP_ID = wandb.sweep(sweep_config, project=WANDB_PROJECT_NAME)
 logging.info(f"Creating Sweep: {sweep_id}")
 
 # let's write the sweep_id on the file as arg for the main script
-train_config["spec"]["replicaSpecs"]["Worker"]["template"]["spec"]["containers"][0]['args'][6] = dq(f'--sweep_id={sweep_id}')
-with open(TRAIN_CONFIG, 'w') as file:
-    ryaml.dump(train_config, file)
+def update_sweep_info(sweep_id, project):
+    "Inject wandb info in train yaml"
+    train_config["spec"]["replicaSpecs"]["Worker"]["template"]["spec"]["containers"][0]['args'][6] = dq(f'--sweep_id={sweep_id}')
+    train_config["spec"]["replicaSpecs"]["Worker"]["template"]["spec"]["containers"][0]['args'][7] = dq(f'--project={project}')
+    with open(TRAIN_CONFIG, 'w') as file:
+        ryaml.dump(train_config, file)
 
+update_sweep_info(SWEEP_ID, WANDB_PROJECT_NAME)
 
 # we probably don't need this anymore....
 cli_cmd = ['kubectl','apply','-f', TRAIN_CONFIG]
 logging.info(f"Running command: {cli_cmd}")
 
-# subprocess.run(cli_cmd)
-    
+
+subprocess.run(cli_cmd)
+subprocess.run(cli_cmd)
+subprocess.run(cli_cmd)
+subprocess.run(cli_cmd)
+subprocess.run(cli_cmd)
